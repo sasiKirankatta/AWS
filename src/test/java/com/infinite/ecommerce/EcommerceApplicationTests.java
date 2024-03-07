e9d33b2b6c280eb5e8c517fb5b43e31f72025031
Sasi <sasikirankatta10@gmail.com>
2024-03-07 10:33:52 +0530

    Aws

---
+++ src/test/java/com/infinite/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinite.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
